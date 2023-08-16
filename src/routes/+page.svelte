5c41f49703f08b69dac0b0873e224643f770b9c3
stef <stefthedoggo4@gmail.com>
2023-08-16 19:42:12 +0100

    starting things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,136 +5,273 @@
 	onMount(() => {
 		new Vivus('box', { duration: 500, type: 'oneByOne' }, () => {});
 	});
+
+	/**
+	 * @param {string} className
+	 */
+	 function buttonPressed(className) {
+		let theDogs = document.getElementsByClassName(className);
+		for (let dog of theDogs) {
+			dog.classList.add('active');
+			dog.classList.remove('cube-shadow');
+		}
+	}
+	/**
+	 * @param {string} className
+	 */
+	function buttonReleased(className) {
+		let theDogs = document.getElementsByClassName(className);
+		for (let dog of theDogs) {
+			dog.classList.remove('active');
+			dog.classList.add('cube-shadow');
+		}
+	}
 </script>
 
 <div id="container">
 	<div class="boxContainer">
 		<svg
-			viewBox="0 0 22 25"
 			version="1.1"
 			xmlns="http://www.w3.org/2000/svg"
 			xmlns:xlink="http://www.w3.org/1999/xlink"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			fill-rule="evenodd"
-			xml:space="preserve"
-			id="box"
-			class="boxPart"
+			width="427"
+			height="683"
+			id="cube"
+			class="boxPart benis"
+			><defs
+				><clipPath id="MreExmZsqeoV"
+					><path
+						fill="none"
+						stroke="rgb(10,10,20)"
+						d=" M 0 0 L 427 0 L 427 683 L 0 683 L 0 0 Z"
+					/></clipPath
+				></defs
+			><g transform="scale(1,1)" clip-path="url(#MreExmZsqeoV)"
+				><g
+					><path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 226.80714375198227 552.7646461601528 L 397.12459072617037 454.43182230191655 L 397.12459072617037 257.7661745854442 L 226.80714375198227 356.0989984436804 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-bg"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 226.80714375198227 552.7646461601528 L 56.48969677779411 454.43182230191655 L 56.48969677779414 257.7661745854442 L 226.80714375198227 356.0989984436804 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-bg"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 56.48969677779414 257.7661745854442 L 226.80714375198227 356.0989984436804 L 397.12459072617037 257.7661745854442 L 226.80714375198224 159.43335072720805 Z"
+						fill-opacity="0.09803921568627451"
+						id="top-bg"
+					/>
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 73.52144147521295 376.9529220379645 L 151.19257551740878 421.7963721854893 L 151.24421726519193 332.1392872674238 L 209.77539905456345 365.932280829504 L 209.77539905456342 523.2647990026818 L 73.52144147521295 444.59853991609293 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-button-br"
+						on:mousedown={() => buttonPressed('left-shadow-br')}
+						on:keydown={() => buttonPressed('left-shadow-br')}
+						on:mouseup={() => buttonReleased('left-shadow-br')}
+						on:keyup={() => buttonReleased('left-shadow-br')}
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 73.52144147521298 287.2660217429151 L 132.263167849406 321.18057327771834 L 132.33714206942864 389.13780345485446 L 73.52144147521295 355.180542884208 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-button-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 243.83888844940108 365.932280829504 L 302.2579697448923 332.2040085177419 L 302.2579697448923 399.6605531412661 L 243.83888844940108 433.5430289256001 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-button-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 243.83888844940108 455.8765560832039 L 302.2579697448923 422.0799820673956 L 302.2579697448923 489.5365266909198 L 243.83888844940108 523.2647990026819 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-button-bl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 321.69524343156706 478.3144114956058 L 380.0928460287516 444.59853991609293 L 380.0928460287516 287.2660217429151 L 321.69524343156706 320.98189332242794 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-button-r"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 90.55318617263177 257.7661745854442 L 226.80714375198224 179.09991549885524 L 363.0611013313328 257.7661745854442 L 226.80714375198227 336.43243367203314 Z"
+						fill-opacity="0.09803921568627451"
+						id="top-button"
+					/>
+					<!-- 
+						-							-
+						-	Button Pressed Paths	-
+						-							-
+					-->
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 90.55318617263191 434.7652575302692 L 209.77539905456342 503.59823423103444 L 209.77539905456342 523.2647990026818 L 73.52144147521295 444.59853991609293 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-shadow-br-b"
+						class="cube-shadow left-shadow-br"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 90.55318617263191 434.7652575302692 L 90.55318617263191 386.78620442378826 L 73.52144147521295 376.9529220379645 L 73.52144147521295 444.59853991609293 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-shadow-br-bl"
+						class="cube-shadow left-shadow-br"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 90.55318617263171 345.34726049838446 L 132.31572074796367 369.45887107749314 L 132.33714206942864 389.13780345485446 L 73.52144147521295 355.180542884208 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-shadow-tl-b"
+						class="cube-shadow left-shadow-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 90.55318617263171 345.34726049838446 L 90.55318617263174 297.09930412873865 L 73.52144147521298 287.2660217429151 L 73.52144147521295 355.180542884208 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-shadow-tl-l"
+						class="cube-shadow left-shadow-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 226.80714375198224 179.09991549885524 L 226.80714375198224 198.76648027050248 L 107.58493087005056 267.59945697126784 L 90.55318617263177 257.7661745854442 Z"
+						fill-opacity="0.09803921568627451"
+						id="top-shadow-l"
+						class="cube-shadow top-shadow"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 226.80714375198224 179.09991549885524 L 226.80714375198224 198.76648027050248 L 346.029356633914 267.5994569712678 L 363.0611013313328 257.7661745854442 Z"
+						fill-opacity="0.09803921568627451"
+						id="top-shadow-r"
+						class="cube-shadow top-shadow"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 380.0928460287516 444.59853991609293 L 380.0928460287516 287.2660217429151 L 363.0611013313328 297.09930412873865 L 363.0611013313329 434.7652575302694 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-r-r"
+						class="cube-shadow right-shadow-r"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 380.0928460287516 444.59853991609293 L 321.69524343156706 478.3144114956058 L 321.69524343156706 458.64784672395854 L 363.0611013313329 434.7652575302694 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-r-b"
+						class="cube-shadow right-shadow-r"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 302.2579697448923 332.2040085177419 L 285.22622504747346 342.0372909035655 L 285.22622504747346 389.8272707554425 L 302.2579697448923 399.6605531412661 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-tl-r"
+						class="cube-shadow right-shadow-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 302.2579697448923 399.6605531412661 L 243.83888844940108 433.5430289256001 L 243.83888844940108 413.72226068138093 L 285.22622504747346 389.8272707554425 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-tl-b"
+						class="cube-shadow right-shadow-tl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 302.2579697448923 422.0799820673956 L 285.22622504747346 431.9331774193241 L 285.2262250474735 479.7032443050961 L 302.2579697448923 489.5365266909198 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-bl-r"
+						class="cube-shadow right-shadow-bl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 302.2579697448923 489.5365266909198 L 243.83888844940108 523.2647990026819 L 243.83888844940108 503.5982342310346 L 285.2262250474735 479.7032443050961 Z"
+						fill-opacity="0.09803921568627451"
+						id="right-shadow-bl-b"
+						class="cube-shadow right-shadow-bl"
+					/>
+					<path
+						fill="rgb(153,51,0)"
+						stroke="rgb(10,10,20)"
+						paint-order="stroke fill markers"
+						fill-rule="evenodd"
+						d=" M 151.19257551740878 421.7963721854893 L 168.2243202148276 411.9630897996657 L 168.26463794790675 341.9660317303098 L 151.24421726519193 332.1392872674238 Z"
+						fill-opacity="0.09803921568627451"
+						id="left-shadow-br-tl"
+						class="cube-shadow left-shadow-br"
+					/>
+				</g></g
+			></svg
 		>
-			<g id="cube" class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					fill="transparent"
-					points="1.1805,6.83071 1.1805,18.1693 11,23.8386 20.8195,18.1693 20.8195,6.83071 11,1.16142 "
-				/><!-- Cube outline -->
-				<polyline points="1.1805,6.83071 11,12.5 20.8195,6.83071 " /><!-- Top Connector -->
-				<polyline points="11,12.5 11,23.8386 " /><!-- Bottom Connector -->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeTop0"
-					fill="transparent"
-					points="11,2.15476 2.81708,6.83071 11,11.3661 18.937,6.83071 "
-				/><!-- Top -->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeRight0"
-					fill="transparent"
-					points="15.5006,15.0984 11.8183,17.2244 11.8183,12.9724 15.5006,10.8465 "
-				/><!-- Right / Top Left -->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeRight1"
-					fill="transparent"
-					points="15.5006,20.2953 11.8183,22.4213 11.8183,18.1693 15.5006,16.0433 "
-				/><!-- Right / Bottom Left-->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeRight2"
-					fill="transparent"
-					points="20.0012,8.24803 16.3189,10.374 16.3189,14.626 16.3189,19.8228 20.0012,17.6969 "
-				/><!-- Right / Right -->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeLeft0"
-					fill="transparent"
-					points="1.99879,12.5 1.99879,8.24803 5.6811,10.374 5.6811,14.626 "
-				/><!-- Left / Top Left -->
-			</g>
-			<g class="cubePart" fill="none" stroke="rgb(10,10,20)" stroke-width="0.07">
-				<polygon
-					id="cubeLeft1"
-					fill="transparent"
-					points="1.99879,17.6969 1.99879,13.4449 6.4994,16.0433 6.4994,10.8465 10.1817,12.9724 10.1817,22.4213 "
-				/><!-- Left / Left -->
-			</g>
-		</svg>
-	</div>
-	<div class="boxContainer">
-		<svg
-			viewBox="0 0 22 26"
-			version="1.1"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			fill-rule="evenodd"
-			xml:space="preserve"
-			id="boxPressed"
-			class="boxPart"
-			style="opacity: 0;"
-		>
-			<g fill="red" stroke="rgb(0,0,0)" stroke-width="0.07">
-				<g
-					><polyline
-						points="14.846,16.9707 14.846,20.4668 15.5006,20.8448 11.8183,22.9707 11.8183,18.7188 15.5006,16.5928 15.5006,20.8448 "
-					/> <polyline points="14.846,20.4668 11.8183,22.2148 " />
-					<!-- Right / Bottom Left --></g
-				>
-				<g
-					><polygon
-						points="20.0012,8.79752 16.3189,10.9235 16.3189,15.1755 16.3189,20.3723 20.0012,18.2463 "
-					/><polyline
-						points="16.3189,19.6164 19.3466,17.8684 20.0012,18.2463 19.3466,17.8684 19.3466,9.17547 "
-					/><!-- Right / Right --></g
-				>
-				<g
-					><polygon points="2.81708,7.3802 11,2.55504 19.1781,7.27946 10.9952,12.1046 " />
-					<polyline points="11,2.55504 11,3.31095 18.5306,7.66129 " />
-					<polyline points="11,3.31095 3.46462,7.75427 " />
-					<!-- Top --></g
-				>
-				<g
-					><polygon
-						points="1.99879,13.0495 1.99879,8.79752 5.6811,10.9235 5.6811,15.1755 "
-					/><polyline points="2.67921,9.19036 2.67921,12.7202 5.6811,14.4534 " />
-					<polyline points="1.99879,13.0495 2.67921,12.7202 " />
-					<!-- Left / Top Left --></g
-				>
-				<g fill="green" style="opacity: 1;">
-					<polyline
-						points="14.846,11.7739 14.846,15.27 15.5006,15.6479 11.8183,17.7739 11.8183,13.5219 15.5006,11.3959 15.5006,15.6479 14.846,15.27 11.8183,17.018 "
-					/><!--Right / Top Left--></g
-				>
-				<g
-					><polyline
-						points="6.4994,16.5928 6.4994,11.3959 10.1817,13.5219 10.1817,22.9707 1.99879,18.2463 1.99879,13.9944 6.4994,16.5928 7.17982,16.2635 7.17982,11.7888 "
-					/><polyline points="1.99879,18.2463 2.67921,17.9171 10.1817,22.2486 " />
-					<polyline points="2.67921,17.9171 2.67921,14.3872 " />
-
-					<!-- Left / L --></g
-				>
-			</g>
-			<g fill="none" stroke="rgb(0,0,0)" stroke-width="0.07">
-				<polyline
-					points="11,24.3881 1.1805,18.7188 1.1805,7.3802 11,13.0495 11,24.3881 20.8195,18.7188 20.8195,7.3802 11,13.0495 "
-				/><!-- Left & Right Fill -->
-				<polyline points="20.8195,7.3802 10.9422,1.61193 1.1805,7.3802 " /><!-- Top Fill-->
-			</g>
-		</svg>
 	</div>
 </div>
 
@@ -163,30 +300,12 @@
 		display: block;
 		margin: auto;
 	}
-	#box {
-		margin-top: 1rem;
-	}
-	#boxPressed {
-		width: 52rem;
-		height: 52rem;
+
+	.cube-shadow {
+		opacity: 0;
 	}
 
-	#cubeTop0:hover {
-		fill: cyan;
-	}
-	#cubeLeft0:hover {
-		fill: red;
-	}
-	#cubeLeft1:hover {
-		fill: green;
-	}
-	#cubeRight0:hover {
-		fill: yellow;
-	}
-	#cubeRight1:hover {
-		fill: blue;
-	}
-	#cubeRight2:hover {
-		fill: orange;
+	.active {
+		opacity: 1 !important;
 	}
 </style>
